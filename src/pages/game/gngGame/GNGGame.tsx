c0257ded5a215ee6e62d569c21e324b245faab0f
Chaipat Chunharas <chaipat.c@chula.ac.th>
2023-08-20 21:48:45 +0200

    Update GNGGame.tsx
    easy :
    {((hitCount + correctRejectionCount) / trialNumber) * 100}
    hard:
    {((hitCount + correctRejectionCount + falseSignalRejectionCount) / trialNumber) * 100}
    ไม่รู้ทำไมถึงไม่เหมือนกัน
    
    แก้เป็น {(hitCount / allGo) * 100}
    
    ส่วน %FA ตอนนี้ใช้ {(falseHitCount / allNoGo) * 100} ตามเดิม

--- src/pages/game/gngGame/GNGGame.tsx
+++ src/pages/game/gngGame/GNGGame.tsx
@@ -624,7 +624,8 @@ function GNGGame(props) {
         </div>
         {isItDone ? 
         <div>
-            {<ScoreSummaryOverlay accuracy={((hitCount + correctRejectionCount) / trialNumber) * 100}  falseHit={(falseHitCount / allNoGo) * 100} easyGNGDone={easyGNGDone} avgHitRt={avgHitRt} refreshPage={refreshPage} backToLandingPage={backToLandingPage}/>}
+            // {<ScoreSummaryOverlay accuracy={((hitCount + correctRejectionCount) / trialNumber) * 100}  falseHit={(falseHitCount / allNoGo) * 100} easyGNGDone={easyGNGDone} avgHitRt={avgHitRt} refreshPage={refreshPage} backToLandingPage={backToLandingPage}/>}
+            {<ScoreSummaryOverlay accuracy={(hitCount / allGo) * 100}  falseHit={(falseHitCount / allNoGo) * 100} easyGNGDone={easyGNGDone} avgHitRt={avgHitRt} refreshPage={refreshPage} backToLandingPage={backToLandingPage}/>}
         </div>
         : null}
         {<RotateAlert />}
